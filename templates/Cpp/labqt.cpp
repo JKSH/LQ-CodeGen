01ba39028ac35e1d550b736290d390935a6933fa
Sze Howe Koh <szehowe.koh@gmail.com>
2014-11-09 13:24:49 +0800

    Add support for registering and posting LabVIEW events in C++

--- templates/Cpp/labqt.cpp
+++ templates/Cpp/labqt.cpp
@@ -61,4 +61,92 @@ stopWidgetEngine()
 	return 0;
 }
 
+qint32 Q_DECL_EXPORT
+registerEventRefs(LVUserEventRef* voidRef, LVUserEventRef* boolRef, LVUserEventRef* i32Ref, LVUserEventRef* stringRef)
+{
+	if (!bridge)
+		return -1;
+
+	bridge->registerEventRef_void(voidRef);
+	bridge->registerEventRef_bool(boolRef);
+	bridge->registerEventRef_i32(i32Ref);
+	bridge->registerEventRef_string(stringRef);
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+registerEventRef_bool(LVUserEventRef* ref)
+{
+	if (!bridge)
+		return -1;
+
+	bridge->registerEventRef_bool(ref);
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+registerEventRef_i32(LVUserEventRef* ref)
+{
+	if (!bridge)
+		return -1;
+
+	bridge->registerEventRef_i32(ref);
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+registerEventRef_string(LVUserEventRef* ref)
+{
+	if (!bridge)
+		return -1;
+
+	bridge->registerEventRef_string(ref);
+	return 0;
+}
+
+// LabVIEW needs to prepend "2" to the string
+qint32 Q_DECL_EXPORT
+connect_void(quint32 qobject, const char* encodedSignal)
+{
+	if (!bridge)
+		return -1;
+
+	QObject::connect((QObject*)qobject, encodedSignal,
+			bridge, SLOT(postLVEvent_void()));
+
+	// TODO: Return error code if connection failed
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+connect_bool(quint32 qobject, const char* encodedSignal)
+{
+	if (!bridge)
+		return -1;
+
+	QObject::connect((QObject*)qobject, encodedSignal,
+			bridge, SLOT(postLVEvent_bool(bool)));
+
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+connect_i32(quint32 qobject, const char* encodedSignal)
+{
+	if (!bridge)
+		return -1;
+
+	QObject::connect((QObject*)qobject, encodedSignal,
+			bridge, SLOT(postLVEvent_i32(int)));
+
+	return 0;
+}
+qint32 Q_DECL_EXPORT
+connect_string(quint32 qobject, const char* encodedSignal)
+{
+	qDebug() << "string" << encodedSignal;
+	if (!bridge)
+		return -1;
+
+	QObject::connect((QObject*)qobject, encodedSignal,
+			bridge, SLOT(postLVEvent_string(QString)));
+
+	return 0;
+}
+
 //[TEMPLATE]
--- templates/Cpp/labqt.h
+++ templates/Cpp/labqt.h
@@ -10,6 +10,12 @@ extern "C" {
 extern qint32 Q_DECL_EXPORT startWidgetEngine();
 extern qint32 Q_DECL_EXPORT stopWidgetEngine();
 
+extern qint32 Q_DECL_EXPORT registerEventRefs(LVUserEventRef* voidRef, LVUserEventRef* boolRef, LVUserEventRef* i32Ref, LVUserEventRef* stringRef);
+extern qint32 Q_DECL_EXPORT connect_void(quint32 qobject, const char* encodedSignal);
+extern qint32 Q_DECL_EXPORT connect_bool(quint32 qobject, const char* encodedSignal);
+extern qint32 Q_DECL_EXPORT connect_i32(quint32 qobject, const char* encodedSignal);
+extern qint32 Q_DECL_EXPORT connect_string(quint32 qobject, const char* encodedSignal);
+
 // TODO: Add QObject::setProperty() and QObject::property()
 
 //[TEMPLATE]
