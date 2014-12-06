5c3a264e95516566181a2fcc59573e2e26e3418b
Sze Howe Koh <szehowe.koh@gmail.com>
2014-12-07 07:40:52 +0800

    [Cpp] Convert the TypeConv class into a namespace
    It was completely static. Don't need any class features.

--- src/Cpp/typeconv.h
+++ src/Cpp/typeconv.h
@@ -4,9 +4,8 @@
 #include <QString>
 class QJsonArray;
 
-class TypeConv
+namespace TypeConv
 {
-public:
 	enum Category {
 		Invalid,
 		Void,
@@ -19,20 +18,20 @@ public:
 		FullContainer
 	};
 
-	static void init(const QJsonArray& conversions, Category category);
+	void init(const QJsonArray& conversions, Category category);
 
-	static Category category(const QString& qtType);
-	static QString typeBase(const QString& qtType);
-	static QString bridgeType(const QString& qtType);
-	static QString dllType(const QString& qtType);
+	Category category(const QString& qtType);
+	QString typeBase(const QString& qtType);
+	QString bridgeType(const QString& qtType);
+	QString dllType(const QString& qtType);
 
-	static QString instanceType_dll(const QString& qtType);
-	static QString instanceType_bridge(const QString& qtType);
+	QString instanceType_dll(const QString& qtType);
+	QString instanceType_bridge(const QString& qtType);
 
-	static QString convCode_bridge2Dll(const QString& qtType);
-	static QString convCode_dll2Bridge(const QString& qtType);
-	static QString convCode_qt2Bridge(const QString& qtType);
-	static QString convCode_bridge2Qt(const QString& qtType);
+	QString convCode_bridge2Dll(const QString& qtType);
+	QString convCode_dll2Bridge(const QString& qtType);
+	QString convCode_qt2Bridge(const QString& qtType);
+	QString convCode_bridge2Qt(const QString& qtType);
 };
 
 #endif // TYPECONV_H
