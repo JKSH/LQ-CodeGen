0fce5776579ca4df0efe78e2746eca7da6e64853
Sze Howe Koh <szehowe.koh@gmail.com>
2020-12-24 09:38:40 +0800

    Bump copyright year

--- templates/Cpp/lqlibinterface.h
+++ templates/Cpp/lqlibinterface.h
@@ -1,5 +1,5 @@
 /*\
- * Copyright (c) 2016 Sze Howe Koh
+ * Copyright (c) 2020 Sze Howe Koh
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
